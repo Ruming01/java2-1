cb0d64b2366e3f8f82394c680cc2aa47b4962d81
Ruming01 <youhyun0609@naver.com>
2024-04-19 11:28:05 +0900

    0419

--- src/App.java
+++ src/App.java
@@ -1,5 +1,9 @@
+import java3.*;
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+
+        Calc bar2 = new Calc();
+        System.out.println(bar2.add2( 3, 6 ));
     }
 }
---
+++ src/java3/Calc.java
@@ -0,0 +1,7 @@
+package java3;
+
+public class Calc {
+    public int add2 (int x, int y) {
+        return ( x + y );
+    }
+}
